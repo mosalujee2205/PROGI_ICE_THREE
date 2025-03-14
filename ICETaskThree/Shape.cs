ccadafc19e101266999fc4e1d53cf18f753dee95
muhammad salujee <st10451769@vcconnect.edu.za>
2025-03-14 17:53:27 +0200

    Added the Shape class

---
+++ ICETaskThree/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICETaskThree
+{
+    class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Shape: {Name}");
+        }
+
+    }
+}
