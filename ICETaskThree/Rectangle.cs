517e86798883eefb07b4887853aaa129a9e4705d
muhammad salujee <st10451769@vcconnect.edu.za>
2025-03-14 17:57:54 +0200

    added rectangle class

---
+++ ICETaskThree/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICETaskThree
+{
+    class Rectangle : Shape, ICalculateArea
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(string name, double width, double height) : base(name)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Width: {Width}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Area: {CalculateArea()}");
+        }
+    }
+    {
+    }
+}
