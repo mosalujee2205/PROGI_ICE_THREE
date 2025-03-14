49c5123c1e5e1e630ffeb8e981508c189c876317
muhammad salujee <st10451769@vcconnect.edu.za>
2025-03-14 17:59:34 +0200

    added circle class

---
+++ ICETaskThree/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICETaskThree
+{
+    class Circle : Shape, ICalculateArea
+    {
+        public double Radius { get; set; }
+        public Circle(string name, double radius) : base(name)
+        {
+            this.Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Radius: {Radius}");
+            Console.WriteLine($"Area: {CalculateArea()}");
+        }
+    }
+
+    }
+
